89a53400ba55b6be7f92fe54f09eb26c5b4be14a
Your Namemai <phungtuyet234@gmail.com>
2018-09-27 09:38:59 +0700

    1st

--- Test.java
+++ Test.java
@@ -7,6 +7,7 @@ public class Test{
         listWord.setWord(word);
         listWord.setWord(word);
         listWord.setWord(word);
+	listWord.setWord(word);
         listWord.showAllWords();
     }
 }
