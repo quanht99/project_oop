f79a3bd5d31a9bc329d58bccd4bc236f84875f3a
tienquan1999 <zed.quan99@gmail.com>
2018-10-18 14:29:50 +0700

    abc

--- src/JavaFx/editWordController.java
+++ src/JavaFx/editWordController.java
@@ -3,17 +3,31 @@ package JavaFx;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.web.HTMLEditor;
 
+import java.sql.SQLException;
+
+import static controller.controller.*;
+
 public class editWordController {
-//    @FXML
-//    private TextArea textArea = new TextArea();
-//    @FXML
-//    HTMLEditor htmlEditor = new HTMLEditor();
-//    @FXML
-//    public void handlebuttonHTML(ActionEvent e)
-//    {
-//        textArea.setText(htmlEditor.getHtmlText());
-//    }
+
+    @FXML
+    private TextField textFieldEdit = new TextField();
+    @FXML
+    private HTMLEditor htmlEditorEdit = new HTMLEditor();
+
+    @FXML
+    public void handleButtonFind(ActionEvent event) throws SQLException {
+        String textFind = textFieldEdit.getText();
+        htmlEditorEdit.setHtmlText(findWord(textFind));
+
+    }
+    @FXML
+    public void handleButtonEdit(ActionEvent e)
+    {
+        editDetail(textFieldEdit.getText(), htmlEditorEdit.getHtmlText());
+    }
+
 }
 
--- src/controller/controller.java
+++ src/controller/controller.java
@@ -102,18 +102,59 @@ public class controller {
         return null;
     }
 
-    public static void deletaWordFromDatabase(String word){
+    public static String deletaWordFromDatabase(String word){
         word  = word.toLowerCase();
         Connection conn = connectWithDatabase.getConnection();
         assert conn != null;
         String sql = "delete from dictionary where word = ?";
+        String check = "select word from dictionary where word = ?";
         try {
-            PreparedStatement pstm = conn.prepareStatement(sql);
+            PreparedStatement pstm = conn.prepareStatement(check);
+            pstm.setString(1, word);
+            ResultSet rs = pstm.executeQuery();
+            String abc = "";
+            while (rs.next()) {
+                abc = rs.getString("word");
+            }
+            if(abc.equals("")){
+                System.out.println("Từ này đã có trong database");
+                return "false";
+            }
+            pstm = conn.prepareStatement(sql);
             pstm.setString(1, word);
             pstm.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return "true";
+    }
+
+    public static String editDetail(String word, String detail){
+        word = word.toLowerCase();
+        Connection con = connectWithDatabase.getConnection();
+        String sql = "update dictionary set detail = ? where word = ?";
+        String check = "select word from dictionary where word = ?";
+        try {
+            assert con != null;
+            PreparedStatement pstm = con.prepareStatement(check);
+            pstm.setString(1, word);
+            ResultSet rs = pstm.executeQuery();
+            String abc = "";
+            while (rs.next()) {
+                abc = rs.getString("word");
+            }
+            if(abc.equals("")){
+                System.out.println("Từ này chưa có trong database");
+                return "false";
+            }
+            pstm = con.prepareStatement(sql);
+            pstm.setString(1, detail);
+            pstm.setString(2, word);
+            pstm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "true";
     }
 
     public static void textToSpeech(String text){
